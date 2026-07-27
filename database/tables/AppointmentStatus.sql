cdb8d32fdd472d96484cf174b9f2502b979fa4e0
karloz <carlos.chavez@gmail.com>
2026-07-27 14:59:39 -0600

    sql script correction

--- database/tables/AppointmentStatus.sql
+++ database/tables/AppointmentStatus.sql
@@ -1,4 +1,4 @@
-IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AppointmentStatuses]') AND type = N'U')
+IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AppointmentStatus]') AND type = N'U')
 BEGIN
 	CREATE TABLE [dbo].[AppointmentStatus]
 	(
@@ -7,3 +7,6 @@ BEGIN
 		[IsActive] BIT NOT NULL CONSTRAINT [DF_AppointmentStatuses_IsActive] DEFAULT (1)
 	);
 END
+
+
+select * from AppointmentStatus
